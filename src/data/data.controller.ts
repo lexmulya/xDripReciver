060deced607960c38670e1c091e66ed409e836d7
Alexey Mulyukin <lexmulya@yandex.ru>
2021-08-15 18:17:45 +0300

    idTransmitter in getData

--- src/data/data.controller.ts
+++ src/data/data.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpStatus, Query, Res } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param, Query, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { DataService } from './data.service';
 
@@ -7,11 +7,11 @@ export class DataController {
   constructor(private readonly dataService: DataService) {
   }
 
-  @Get('')
-  async getData(@Query() query/*, @Res() res: Response*/){
+  @Get(':did')
+  async getData(@Query() query, @Param('did') idTransmitter: string/*, @Res() res: Response*/){
     console.log("get data = ", JSON.stringify(query));
 
-    const ret = await this.dataService.getData();
-    return ret;
+    const ret = await this.dataService.getData(idTransmitter);
+    return JSON.stringify(ret);
   }
 }
--- src/data/data.service.ts
+++ src/data/data.service.ts
@@ -6,22 +6,50 @@ import { DataToPhoneDto } from '../DTO/DataToPhone.dto';
 
 @Injectable()
 export class DataService {
+  readonly idTxCode = ['0', '1', '2', '3', '4', '5', '6', '7',
+  '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+  'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P',
+  'Q', 'R', 'S', 'T', 'U', 'W', 'X', 'Y'];
   constructor(@InjectRepository(Data)
               private dataRepository: Repository<Data>) {
   }
 
-  public async getData(): Promise<DataToPhoneDto> {
+  public async getData(idTransmitter:string): Promise<DataToPhoneDto> {
     try {
-      const lastDataDB = await this.dataRepository.findOneOrFail({
+      const txIdFromString = this.getTxId(idTransmitter);
+      const lastDataDB = await this.dataRepository.findOne({
+        where:{
+          idTransmitter: txIdFromString
+        },
         order:{
           id: 'DESC'
         }
       })
-      const ret = DataToPhoneDto.formatFromDB(lastDataDB);
-      return ret;
+      if(lastDataDB){
+        const ret = DataToPhoneDto.formatFromDB(lastDataDB);
+        return ret;
+      }else {
+        return new DataToPhoneDto();
+      }
+
     } catch (e) {
       console.log('getData e = ', e);
       throw new HttpException("", HttpStatus.NOT_FOUND);
     }
+  }
+
+  private getTxId(strTransmitter: string):number{
+    let src = 0;
+    src |= this.getCode(strTransmitter[0]) << 20;
+    src |= this.getCode(strTransmitter[1]) << 15;
+    src |= this.getCode(strTransmitter[2]) << 10;
+    src |= this.getCode(strTransmitter[3]) << 5;
+    src |= this.getCode(strTransmitter[4]) << 0;
+
+    return src;
+  }
+  private getCode(char){
+    const index = this.idTxCode.indexOf(char);
+    return ( index );
   }
 }
